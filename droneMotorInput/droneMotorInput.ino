fc8f995d34cc3438164fe486e1a0ee32bd255b17
CarterTheGreat <cdwlions2016@gmail.com>
2019-01-13 21:24:47 -0500

    Added motor on and off, pwm mapping

--- droneMotorInput/droneMotorInput.ino
+++ droneMotorInput/droneMotorInput.ino
@@ -1,10 +1,9 @@
 
 char key;
 String data = "";
-String xS,yS,zS;
+String runningS,xS,yS,zS;
 double x,y,z;
-
-int startInd, ind1, ind2, endInd;
+int startInd, ind1, ind2, ind3, endInd;
 
 double pos[2];
 
@@ -28,13 +27,35 @@ double FLOut;
 double RLOut;
 double RROut;
 
+int FRPin = 5;
+int FLPin = 6;
+int RLPin = 9;
+int RRPin = 10;
+
 void setup() {
   Serial.begin(9600);
   delay(1000);
+
+  analogWrite(FRPin, 0);
+  analogWrite(FLPin, 0);
+  analogWrite(RLPin, 0);
+  analogWrite(RRPin, 0);
 }
 
 void loop() {
 //Input Allocation---------------------------------------------
+/*
+ * Input Format
+ * <motorsRunning/xxx/yyy/zzz>
+ * ex. 
+ *  <1/255/255/255>
+ *  <0/0/0/0>
+ */
+
+
+
+
+
   while(Serial.available()){
     key = Serial.read();
     data.concat(key);
@@ -43,16 +64,19 @@ void loop() {
       startInd = data.indexOf('<');
       ind1 = data.indexOf('/');
       ind2 = data.indexOf('/',ind1+1);
+      ind3 = data.indexOf('/',ind2+1);
       endInd = data.indexOf('>');
 
-      xS = data.substring(startInd+1,ind1);
-      yS = data.substring(ind1+1,ind2);
-      zS = data.substring(ind2+1,endInd);
-      
-      //Serial.println("Data: |"+data+"|");
-      //Serial.println("x: |"+xS+"|");
-      //Serial.println("y: |"+yS+"|");
-      //Serial.println("z: |"+zS+"|");
+      runningS = data.substring(startInd+1,ind1);
+      xS = data.substring(ind1+1,ind2);
+      yS = data.substring(ind2+1,ind3);
+      zS = data.substring(ind3+1,endInd);
+
+      Serial.println(startInd);Serial.println(ind1);Serial.println(ind2);Serial.println(ind3);Serial.println(endInd);
+      Serial.println("Data: "+data);
+      Serial.println("x: "+xS);
+      Serial.println("y: "+yS);
+      Serial.println("z: "+zS);
 
 
         x = xS.toDouble();
@@ -70,38 +94,34 @@ void loop() {
         FLDist = sqrt(sq(x-frontLeft[0]) + sq(y-frontLeft[1]));
         RLDist = sqrt(sq(x-rearLeft[0]) + sq(y-rearLeft[1]));
         RRDist = sqrt(sq(x-rearRight[0]) + sq(y-rearRight[1])); 
-        /*
+        
         Serial.println(FRDist);
         Serial.println(FLDist);
         Serial.println(RLDist);
         Serial.println(RRDist);
-        */
+        
         
         //Front Right
         if(FRDist<1)FRDist = 1;
         FRScale = FRDist/735.39;
-        FRScale = 1-FRScale;
         FROut = 255*FRScale;
         if(FROut < 75) FROut = 75;
         
         //Front Left
         if(FLDist<1)FLDist = 1;
         FLScale = FLDist/735.39;
-        FLScale = 1-FLScale;
         FLOut = 255*FLScale;
         if(FLOut < 75) FLOut = 75;
 
         //Rear Left
         if(RLDist<1)RLDist = 1;
         RLScale = RLDist/735.39;
-        RLScale = 1-RLScale;
         RLOut = 255*RLScale;
         if(RLOut < 75) RLOut = 75;
         
         //Rear Right
         if(RRDist<1)RRDist = 1;
         RRScale = RRDist/735.39;
-        RRScale = 1-RRScale;
         RROut = 255*RRScale;
         if(RROut < 75) RROut = 75;
         /*
@@ -109,16 +129,25 @@ void loop() {
 `       */
 
         Serial.println("Outputs");
+        Serial.println("---------");
+        Serial.println(runningS);
         Serial.println(FROut);
         Serial.println(FLOut);
         Serial.println(RLOut);
         Serial.println(RROut);
-          
-        x = 0;
-        y = 0;
-        z = 0;
-        FRDist = 0;
-        data = "";
+        Serial.println("---------");
+  
+        if(runningS == "1"){
+          Serial.println("Outputs sent");
+          analogWrite(FRPin, FROut);
+          analogWrite(FLPin, FLOut);
+          analogWrite(RLPin, RLOut);
+          analogWrite(RRPin, RROut);
+          delay(20);
+        }else Serial.println("No outputs sent");
+
+       runningS == "0"; 
+       data = "";
      }      
   }
   
